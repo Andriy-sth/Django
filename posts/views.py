38fdb72c209c88b242d101968c5e5f3ca8fdd893
Andriy-sth <sht.andriy@gmail.com>
2017-05-10 19:28:55 +0300

    add post form

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
 def post_create(request):
-    return HttpResponse('<h1> Hi !!<h1>')
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'post_form.html', context)
 
 
 def post_detail(request, id=None):
